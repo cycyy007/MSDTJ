5f06a995e0b3b3af4e5d5adaa65128e1f8d92adf
chen <chen@chendeMacBook-Pro.local>
2015-08-28 17:38:19 +0800

    Initial Commit

---
+++ MSDTJ/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MSDTJ
+//
+//  Created by chen on 15/8/28.
+//  Copyright (c) 2015年 Benfu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
